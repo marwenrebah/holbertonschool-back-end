da39b09160f8da509e0f632d9ecd69d8665d73a5
marwenrebah <6863@holbertonstudents.com>
2024-04-17 09:19:43 -1100

    Gather data from an API

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -12,17 +12,19 @@ def TODO_PROGRESS():
     Retrieves To Do list progress for a given employee.
     """
     user_id = sys.argv[1]
-    user_url = f'https://jsonplaceholder.typicode.com/users/{user_id}'
-    todos_url = f'https://jsonplaceholder.typicode.com/todos/?userId={user_id}'
-    user_response = requests.get(user_url).json()
-    name = user_response.get('name')
-    todos_response = requests.get(todos_url).json()
+    user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
+    todos_url = 'https://jsonplaceholder.typicode.com/todos/?userId={}'.format(
+        user_id)
+    name = requests.get(user_url).json().get('name')
+    todo_response = requests.get(todos_url).json()
     tasks = [task.get('title')
-             for task in todos_response if task.get('completed') is True]
+             for task in todo_response if task.get('completed')]
+
     print('Employee {} is done with tasks ({}/{}):'.format(name,
-          len(tasks), len(todos_response)))
+          len(tasks), len(todo_response)))
     print('\n'.join('\t {}'.format(task) for task in tasks))
 
 
 if __name__ == "__main__":
-    TODO_PROGRESS()
+    if len(sys.argv) == 2:
+        TODO_PROGRESS()
